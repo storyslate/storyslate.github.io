07b41fdcf76284ccf768192c35c27ec00167d314
StorySlate <oneman@storyslate.in>
2025-10-03 16:47:54 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,10 @@
 document.addEventListener('DOMContentLoaded', function() {
+    // Get the current year using JavaScript
+    const currentYear = new Date().getFullYear();
+
+    // Set the current year in the HTML element with id "current-year"
+    document.getElementById("current-year").textContent = currentYear;
+    
     const greetingElement = document.getElementById('greeting');
     greetingElement.textContent = 'Welcome to the Static Web App!';
 
